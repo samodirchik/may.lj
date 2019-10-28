62199af5e228f1e9c5e7066100f20097079c2bd4
Someone <rgbjkbs@hdbf.ru>
2019-10-28 20:29:55 +0200

    routing

--- app/core/Route.php
+++ app/core/Route.php
@@ -3,9 +3,49 @@
 namespace core;
 
 class Route {
-    static public function init(){
-       //TODO create base routing
-       echo 'the site is working';
+    
+    
+/**
+ * routing /controller/action
+ */
+    static public function init() {
+        $controllerName = 'main'; //это значение останется в переменной если не произойдет перезапись
+        $actionName = 'index'; //это значение останется в переменной если не произойдет перезапись
+        $routeItems = explode('/', $_SERVER['REQUEST_URI']); //забирем дополнительный путь документа в домене и разделяем его на список
+        array_shift($routeItems); //0-й элемент всегда содержит пустую строку поэтому удаляем его 
+        //проверяем последний элемент массива на пустое значение
+        //при необходимости удалем его
+
+        if (empty($routeItems[count($routeItems) - 1])) {
+            array_pop($routeItems);
+        }
+        //если в массиве больше двух элементов ,то 404
+        if (count($routeItems) > 2) {
+            self::error404();
+        }
+
+        if (!empty($routeItems[0])) {
+            //$controllerName = mb_strtolower(urldecode($routeItems[0]));//для кириллицы
+            $controllerName = strtolower($routeItems[0]);
+        }
+        if (!empty($routeItems[1])) {
+            //$actionName = mb_strtolower(urldecode($routeItems[1]));//для кириллицы
+            $actionName = strtolower($routeItems[1]);
+        }
+        //$controllerClassName = mb_convert_case($controllerName, MB_CASE_TITLE);//для кириллицы
+        $controllerClassName = 'controllers\\' . ucfirst($controllerName) . 'Controller';
+        if (!class_exists($controllerClassName)) {
+            self::error404();
+        }
+        $controller = new $controllerClassName();
+        if (!method_exists($controller, $actionName)) {
+            self::error404();
+        }
+        $controller->$actionName();
     }
-      
+
+    static public function error404() {
+        exit('404');
+    }
+
 }
