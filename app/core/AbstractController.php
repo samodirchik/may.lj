c81b478085469a71124ae727ed5713a5a37f12cd
Someone <rgbjkbs@hdbf.ru>
2019-10-28 20:36:06 +0200

    add properties and abstract index action

--- app/core/AbstractController.php
+++ app/core/AbstractController.php
@@ -4,4 +4,23 @@ namespace core;
 
 abstract class AbstractController {
     
+    /**
+     *
+     * @var Abstractmodel
+     */
+    public $model;
+    
+    /**
+     *
+     * @var View
+     */
+    public $view;
+    
+    public function __construct() {
+        $this->view = new View();
+    }
+    
+
+
+    abstract public function index();
 }
