5041b85befad963eaa09d5fb104b06f0ee84ce0b
Someone <rgbjkbs@hdbf.ru>
2019-10-30 19:01:34 +0200

    add function to get full url

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,14 +5,28 @@
  * 
  * @return string app path from server root
  */
-function getAppPath(){
-    return $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR;
+function getAppPath() {
+    return $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR;
 }
 
-spl_autoload_register(function($class){
+/**
+ * Возвращает указанный путь га текущем сайте 
+ * 
+ * @param string $path дополнительный путь сайта
+ */
+function url(string $path = null) {
+    if ($_SERVER['SERVER_PORT'] === '80') {
+        $sheme = 'http';
+    } else if ($_SERVER['SERVER_PORT'] === '443') {
+        $sheme = 'https';
+    }
+    return $sheme . '://' . $_SERVER['HTTP_HOST'] . $path;
+}
+
+spl_autoload_register(function($class) {
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    $classPath = getAppPath().$class.'.php';
-    if(file_exists($classPath)){
+    $classPath = getAppPath() . $class . '.php';
+    if (file_exists($classPath)) {
         include_once $classPath;
         return true;
     }
