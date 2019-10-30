defbb233c4dbfb18c02924ee8506688738fbf022
Someone <rgbjkbs@hdbf.ru>
2019-10-30 20:01:05 +0200

    create registration page

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -9,7 +9,9 @@ class AuthController extends AbstractController {
     public function index() {
         $this->view->render('auth_index_view');
     }
-    
+
+    public function registration() {
+        $this->view->render('auth_register_view');
+    }
+
 }
-
-
