dcb3e8ec4ebdfded828df54ccf55ec726eaa5cdd
Someone <rgbjkbs@hdbf.ru>
2019-11-04 20:34:30 +0200

    show posts

--- app/views/posts_index_view.php
+++ app/views/posts_index_view.php
@@ -1,1 +1,7 @@
-lol
+<?php foreach ($this->posts as $postItem) : ?>
+<article>
+    <h2><a href="<?= url('/posts/item?id=' . $postItem['id']) ?>"><?= $postItem['title'] ?></a></h2>
+<div><?= $postItem['text'] ?></div>  
+<div>Avtor: <?= $postItem['author']?></div>
+</article>
+<?php endforeach;?>
